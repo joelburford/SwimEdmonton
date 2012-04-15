f428e8d4832289014e87cd8b0c602416c72acee3
Derek Dowling <dowling@ualberta.ca>
2012-04-14 23:23:33 -0600

    started work on location finder

--- SwimEdmonton/src/utils/FacilityFinder.java
+++ SwimEdmonton/src/utils/FacilityFinder.java
@@ -1,5 +1,21 @@
 package utils;
 
+import java.util.ArrayList;
+
+import model.Facility;
+
 public class FacilityFinder {
+	
+	public static Facility findClosestFacility(ArrayList<Facility> facilities) {
+	
+		Facility selected = null;
+		
+		
+		
+		return selected;
+		
+	}
+	
+	
 
 }
